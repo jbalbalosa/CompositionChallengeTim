6f82d8f1556686fdcae1e865420495f184504345
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-06 11:41:31 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+  public static void main(String[] args) {
+        System.out.println("Composition Challenge Solution by Tim!");
+        System.out.println("Smart Kitchen");
+
+        SmartKitchen kitchen = new SmartKitchen();
+
+//        kitchen.getDishWasher().setHasWorkToDo(true);
+//        kitchen.getIceBox().setHasWorkToDo(true);
+//        kitchen.getBrewMaster().setHasWorkToDo(true);
+//
+//        kitchen.getDishWasher().doDishes();
+//        kitchen.getIceBox().oderFood();
+//        kitchen.getBrewMaster().brewCoffee();
+
+        kitchen.setKitchenState(true,false,true);
+        kitchen.doKitchenWork();
     }
+}
 }
